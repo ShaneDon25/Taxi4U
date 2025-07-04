f368c5bf434a311584d2154b98afb9ea2f97dc55
ShaneDon25 <shanedonwork2017@gmail.com>
2025-07-04 15:21:10 +0100

    I am simplifying the home page before making major changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,10 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { StatusBar } from 'react-native';
-import { View } from 'react-native';
-import { Text } from 'react-native';
+import { StatusBar, Text } from 'react-native';
 import { SafeAreaView} from "react-native-safe-area-context";
 
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-amber-200">
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
         <Text>Hello World</Text>
         <StatusBar style="auto" />
     </SafeAreaView>
